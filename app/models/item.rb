edeab7eba9442c871a749458d5b8985d32b52742
Kameyama92 <77086536+Kameyama92@users.noreply.github.com>
2021-01-27 11:38:12 +0900

    itemのカテゴリー実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  belongs_to :category
+
+  belongs_to :user
+
+  #空の投稿を保存できないようにする
+  validates :title, :text, presence: true
+
+    #ジャンルの選択が「--」の時は保存できないようにする
+  validates :category_id, numericality: { other_than: 1 } 
 end
